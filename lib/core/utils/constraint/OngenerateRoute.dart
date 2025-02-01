9744fc77dd525074acdee5e9da8d49d864b7432d
Ibrahim Mohammed <hemamohamed.hm35@gmail.com>
2025-02-01 20:42:35 +0200

    add bloc provider in manage route at book details

--- lib/core/utils/constraint/OngenerateRoute.dart
+++ lib/core/utils/constraint/OngenerateRoute.dart
@@ -1,10 +1,13 @@
 import 'package:bookly/feature/home/data/model/list_of_book_home_model.dart';
+import 'package:bookly/feature/home/data/repo/home_services_imple.dart';
+import 'package:bookly/feature/home/presentation/manage/details/item_details_categories_cubit.dart';
 import 'package:bookly/feature/home/presentation/view/screen/BookDetails.dart';
 import 'package:bookly/feature/search/presentaion/view/screen/search%20screen.dart';
 import 'package:bookly/feature/splash/presentation/view/screen/Splash.dart';
 import 'package:flutter/material.dart';
 import 'package:bookly/feature/home/presentation/view/screen/home.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 
 class ManagerRoute {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -15,8 +18,11 @@ class ManagerRoute {
         return MaterialPageRoute(builder: (_) => Home());
       case '/bookDetails':
           Item? book ;
-        return MaterialPageRoute(builder: (_) =>BookDetails(
-       fetchItems:book,
+        return MaterialPageRoute(builder: (_) =>BlocProvider(
+          create: (context) => ItemDetailsCategoriesCubit(GetIt.instance.get<HomeRepoImple>())..fetchSimilarBooks(),
+          child: BookDetails(
+                 fetchItems:book,
+          ),
         ));
       case '/search':
         return MaterialPageRoute(builder: (_) => Search());
